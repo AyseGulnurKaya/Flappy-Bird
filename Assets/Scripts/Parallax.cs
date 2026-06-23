0094342ac1f499578291a2764c8fecf7973e8386
AyseGulnurKaya <aysegulnur.kaya@stud.h-da.de>
2026-06-23 23:59:03 +0100

    Tamamlandı

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class Parallax : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    public float animationSpeed = 1f;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0);
+    }
+}
